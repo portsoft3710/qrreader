a9a61404e08646831c0af13af85c2892852a1fdd
portsoft3710 <47545184+portsoft3710@users.noreply.github.com>
2020-07-10 14:00:43 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
-if (!navigator.mediaDevices) {
-    document.querySelector('#js-unsupported').classList.add('is-show')
+if(!navigator.mediaDevices) {
+    document.querySelector('#js-unsupported').classList.add('is-show');
 }
 
-const video  = document.querySelector('#js-video')
-const canvas = document.querySelector('#js-canvas')
-const ctx    = canvas.getContext('2d')
+const video = document.querySelector('#js-video');
+const canvas = document.querySelector('#js-canvas');
+const ctx = canvas.getContext('2d');
+const cover = document.querySelector('#js-cover-img');
 function checkImage(){
-//const checkImage = () => {
+//const checkImage =() => {
 
     // 取得している動画をCanvasに描画
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
     // Canvasからデータを取得
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
     // jsQRに渡す
-    const code = jsQR(imageData.data, canvas.width, canvas.height)
+    const code = jsQR(imageData.data, canvas.width, canvas.height);
 
     // QRコードの読み取りに成功したらモーダル開く
     // 失敗したら再度実行
-    if (code) {
-        openModal(code.data)
-    } else {
-        setTimeout(() => { checkImage() }, 200)
+    if(code){
+        openModal(code.data);
+    }
+	else{
+        setTimeout(() => { checkImage() }, 200);
     }
 }
 
 const openModal = function(url) {
-    document.querySelector('#js-result').innerText = url
-    document.querySelector('#js-link').setAttribute('href', url)
-    document.querySelector('#js-modal').classList.add('is-show')
+    document.querySelector('#js-result').innerText = url;
+    document.querySelector('#js-link').setAttribute('href', url);
+    document.querySelector('#js-modal').classList.add('is-show');
 }
 
 document.querySelector('#js-modal-close')
-    .addEventListener('click', () => {
+    .addEventListener('click',() => {
         document.querySelector('#js-modal').classList.remove('is-show')
-        checkImage()
+        checkImage();
     })
 
 function dispCamera(){
+cover.classList.remove('show');
+video.classList.add('show');
 navigator.mediaDevices
     .getUserMedia({
         audio: false,
@@ -49,14 +53,14 @@ navigator.mediaDevices
         }
     })
     .then(function(stream) {
-        video.srcObject = stream
+        video.srcObject = stream;
         video.onloadedmetadata = function(e) {
-            video.play()
-            checkImage()
+            video.play();
+            checkImage();
         }
     })
     .catch(function(err) {
-        alert('Error!!')
+        alert('Error!!');
     })
 }
 
